4e43e467dbf20c49c0fd72ce1739730722ac991a
Andrew J. Kroll <a@oo.ms>
2013-04-10 01:47:07 -0400

    Clean up code formatting to industry standards.

--- RTClib.h
+++ RTClib.h
@@ -5,54 +5,80 @@
 
 #ifndef RTCLIB_H
 #define RTCLIB_H
+
 class DateTime {
 public:
-    DateTime (uint32_t t =0);
-    DateTime (uint16_t year, uint8_t month, uint8_t day,
-                uint8_t hour =0, uint8_t min =0, uint8_t sec =0);
-    DateTime (const char* date, const char* time);
-    uint16_t year() const       { return 2000 + yOff; }
-    uint8_t month() const       { return m; }
-    uint8_t day() const         { return d; }
-    uint8_t hour() const        { return hh; }
-    uint8_t minute() const      { return mm; }
-    uint8_t second() const      { return ss; }
-    uint8_t dayOfWeek() const;
+        DateTime(uint32_t t = 0);
+        DateTime(uint16_t year, uint8_t month, uint8_t day,
+                uint8_t hour = 0, uint8_t min = 0, uint8_t sec = 0);
+        DateTime(const char* date, const char* time);
 
-    // 32-bit times as seconds since 1/1/2000
-    long secondstime() const;   
-    // 32-bit times as seconds since 1/1/1970
-    uint32_t unixtime(void) const;
+        uint16_t year() const {
+                return 2000 + yOff;
+        }
+
+        uint8_t month() const {
+                return m;
+        }
+
+        uint8_t day() const {
+                return d;
+        }
+
+        uint8_t hour() const {
+                return hh;
+        }
+
+        uint8_t minute() const {
+                return mm;
+        }
+
+        uint8_t second() const {
+                return ss;
+        }
+        uint8_t dayOfWeek() const;
+
+        // 32-bit times as seconds since 1/1/2000
+        long secondstime() const;
+        // 32-bit times as seconds since 1/1/1970
+        uint32_t unixtime(void) const;
 
 protected:
-    uint8_t yOff, m, d, hh, mm, ss;
+        uint8_t yOff, m, d, hh, mm, ss;
 };
 
 // RTC based on the DS1307 chip connected via I2C and the Wire library
-enum Ds1307SqwPinMode { OFF = 0x00, ON = 0x80, SquareWave1HZ = 0x10, SquareWave4kHz = 0x11, SquareWave8kHz = 0x12, SquareWave32kHz = 0x13 };
+
+enum Ds1307SqwPinMode {
+        OFF = 0x00, ON = 0x80, SquareWave1HZ = 0x10, SquareWave4kHz = 0x11, SquareWave8kHz = 0x12, SquareWave32kHz = 0x13
+};
 
 class RTC_DS1307 {
 public:
-  static uint8_t begin(void);
-    static void adjust(const DateTime& dt);
-    static void set(int shour, int smin, int ssec, int sday, int smonth, int syear);
-    uint8_t isrunning(void);
-    static DateTime now();
-    static uint8_t readMemory(uint8_t offset, uint8_t* data, uint8_t length);
-    static uint8_t writeMemory(uint8_t offset, uint8_t* data, uint8_t length);
-    static Ds1307SqwPinMode readSqwPinMode();
-    static void writeSqwPinMode(Ds1307SqwPinMode mode);
+        static uint8_t begin(void);
+        static void adjust(const DateTime& dt);
+        static void set(int shour, int smin, int ssec, int sday, int smonth, int syear);
+        uint8_t isrunning(void);
+        static DateTime now();
+        static uint8_t readMemory(uint8_t offset, uint8_t* data, uint8_t length);
+        static uint8_t writeMemory(uint8_t offset, uint8_t* data, uint8_t length);
+        static Ds1307SqwPinMode readSqwPinMode();
+        static void writeSqwPinMode(Ds1307SqwPinMode mode);
 };
 
 // RTC using the internal millis() clock, has to be initialized before use
 // NOTE: this clock won't be correct once the millis() timer rolls over (>49d?)
+
 class RTC_Millis {
 public:
-    static void begin(const DateTime& dt) { adjust(dt); }
-    static void adjust(const DateTime& dt);
-    static DateTime now();
+
+        static void begin(const DateTime& dt) {
+                adjust(dt);
+        }
+        static void adjust(const DateTime& dt);
+        static DateTime now();
 
 protected:
-    static long offset;
+        static long offset;
 };
 #endif // RTCLIB_H
